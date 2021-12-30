9f017df54a201cc48f93e331bce41b979964632c
dxs4320 <91452167+dxs4320@users.noreply.github.com>
2021-12-30 16:55:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,74 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-let engine;
-let world;
-var ball;
-var ground;
-var wedge;
-var angle=60;
-var poly;
-var boxes=[];
+var ball,groundObj,leftSide,rightSide;
+var world;
+var radius = 70;
+var paper;
+var dustbin;
+
+function preload(){
+
+	dustbinImg = loadImage("dustbin.png");
+	paperImg = loadImage("paper.png");
+
+}
+
 
 function setup() {
-  createCanvas(400,400);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
-  engine = Engine.create();
-  world = engine.world;
-  var option={
-    isStatic:true
-  };
-  ground=Bodies.rectangle(100,300,400,20,option);
-  World.add(world,ground);
-  //box1 = new Box(200,100,50,50);
-  
+	engine = Engine.create();
+	world = engine.world;
 
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
-}
-function mousePressed(){
-  boxes.push(
-    
-    
-   box = new Box(mouseX,mouseY,50,50)
-    
-    
-    )
-}
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		density:0.4
+	}
 
-function draw() 
-{
-  background(51);
-  rect(ground.position.x,ground.position.y,400,10);
-  Engine.update(engine);
- for(var i=0;i<boxes.length;i++) {
- boxes[i].show();}
-  
+	ball = Bodies.circle(260,100,radius/2.6,ball_options);
+	World.add(world,ball);
+
+	ground=new Ground(width/2,670,width,20);
+	leftSide = new Ground(1100,600,10,120);
+	rightSide = new Ground(1270,600,10,120);
+	bottomSide = new Ground(1185,650,150,20);
+
+	Engine.run(engine);
   
 }
 
+
+function draw() {
+	background(200);
+	rectMode(CENTER);
+
+
+	ground.display();
+	leftSide.display();  
+	rightSide.display();
+	bottomSide.display();
+
+	
+	imageMode(CENTER);
+
+    image(paperImg,ball.position.x,ball.position.y,radius,radius);
+
+
+	
+	image(dustbinImg, 1185, 570, 200, 200);
+	
+
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+    
+  	}
+}
